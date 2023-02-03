f97e45bf6e2f071e4f46ea78fb7323d86071b1bc
maurizio <42836292+madmicio@users.noreply.github.com>
2023-02-03 14:03:04 +0100

    Update webos-source-image.js

--- webos-source-image.js
+++ webos-source-image.js
@@ -44,6 +44,17 @@ class WebOsSource extends HTMLElement {
     
         this.innerHTML = `
             <style>
+            .card {
+                background: var(--ha-card-background, var(--card-background-color, white) );
+                border-radius: var(--ha-card-border-radius, 4px);
+                box-shadow: var(--ha-card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) );
+                color: var(--primary-text-color);
+                display: block;
+                transition: all .3s ease-out 0s;
+                position: relative;
+                padding-top: 10px;
+                filter: saturate(var(--color_saturation));
+            }
                 .image-container {
                     padding-top: 56.34%;
                     background-size: cover;
@@ -51,7 +62,9 @@ class WebOsSource extends HTMLElement {
                     transform: scale(0.7);
                 }
             </style>
+            <div class="card">
             <div class="image-container" style="background-image: url(/local/lg_remote/tv_logo/${encodedFileName}.png)"></div>
+            </div>
         `;
     }
     
